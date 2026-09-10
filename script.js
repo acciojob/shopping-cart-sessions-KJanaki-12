9752a3cfb0bbfac88d92007520f5286c4c1cc12f
KJanaki-12 <kenchugundujanaki@gmail.com>
2026-09-10 16:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,29 +9,59 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
+let cartItems = [];
+
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartt = document.getElementById("clear-cart-btn");
+
+clearCartt.addEventListener('click', clearCart);
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} 
+	<button class="add-to-cart-btn" data-id="${product.id}" onClick="addToCart(${product.id})">
+	Add to Cart
+	</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartList.innerHTML = "";
+	cartItems.forEach((item) => {
+		window.sessionStorage.setItem(`ProductName`, `${item.name}`);
+		window.sessionStorage.setItem(`Price`, `${item.price}`);
+		const li = document.createElement("li");
+		li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}" onClick="removeFromCart(${item.id})">
+	Remove From Cart
+	</button>`;
+		cartList.appendChild(li);
+	});
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	cartItems.push(products[productId-1]);
+	renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	cartItems = cartItems.filter(item => item.id !== productId);
+    renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	window.sessionStorage.clear();
+	cartItems = [];
+	cartList.innerHTML = "";
+}
 
 // Initial render
 renderProducts();
